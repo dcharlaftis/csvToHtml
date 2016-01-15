5992dc9dcb8c75767b6edc5cf4960d85c2504dba
dcharlaftis <d.charlaftis@dotbydot.gr>
2016-01-15 18:24:21 +0200

    fg

--- csvToHtml.js
+++ csvToHtml.js
@@ -1,9 +1,41 @@
-var lineReader = require('line-reader');
+var lineReader = require('line-reader'),
+    fs = require("fs");
 
-lineReader.eachLine('example.csv', function(line, last) {
-  console.log(line);
-  // do whatever you want with line...
-  if(last){
-    // or check if it's the last one
-  }
+var delimiter = "," ,
+    first = true,
+    previous = "",
+    inFile="example.csv",
+    outFile="index.html";
+
+//write html header
+var header = "<html><head><title>Template</title></head><body>"
+fs.appendFile(outFile, header, function (err) {
+          if (err){
+          	console.log("error in file writing " , err);            
+          }          
+      });
+
+lineReader.eachLine(inFile, function(line, last) {
+    console.log(line);
+    //avoid the first line - title
+    if (!first){
+    	 var arr_line = line.split(delimiter);
+
+    }
+   
+
+
+    previous = line;
+    first = false;
+    if (last) {
+        // or check if it's the last one
+    }
 });
+
+//write footer
+var footer = "</body></html>"
+fs.appendFile(outFile, footer, function (err) {
+          if (err){
+          	console.log("error in file writing " , err);            
+          }          
+      });
