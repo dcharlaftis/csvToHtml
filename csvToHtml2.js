72c261ed8ba3814e4a80f9c1530bad6d7776cf5d
dcharlaftis <d.charlaftis@dotbydot.gr>
2016-01-18 17:26:47 +0200

    tyty

--- csvToHtml2.js
+++ csvToHtml2.js
@@ -1,14 +1,14 @@
-var fs = require("fs");
+var fs = require('fs');
 
-var delimiter = ",",
+var delimiter = '$',
     first = true,
-    previous = "",
-    inFile = "example.csv",
-    outFile = "index.html";
+    previous = '',
+    inFile = 'example4.csv',
+    outFile = 'index.html';
 
-var stateA_prev = "Null",
-    stateB_prev = "Null",
-    stateC_prev = "Null";
+var stateA_prev = 'Null',
+    stateB_prev = 'Null',
+    stateC_prev = 'Null';
 
 var first = true;
 
@@ -17,7 +17,7 @@ var arr_line;
 function writetoHtml(str) {
     fs.appendFile(outFile, str, function(err) {
         if (err) {
-            console.log("Error in file writing ", err);
+            console.log('Error in file writing ', err);
         }
     });
 }
@@ -29,7 +29,7 @@ var line;
 var lineNumber = 0;
 
 //write html header
-var header = "<html><head><title>Template</title></head><body>"
+var header = '<html><head><title>Template</title></head><body>'
 writetoHtml(header);
 
 function stateChange(A, B, C, preA, preB, preC) {
@@ -53,20 +53,20 @@ function stateChange(A, B, C, preA, preB, preC) {
 }
 
 function closeDivs(stateArray) {
-    var result = "";
+    var result = '';
     for (var i = 0; i < stateArray.length; i++) {
-        if (stateArray[i][1] != "Null")
-            result += "</div>";
+        if (stateArray[i][1] != 'Null')
+            result += '</div>';
     }
     return result;
 
 }
 
 function openDivs(stateArray) {
-    var result = "";
+    var result = '';
     for (var i = 0; i < stateArray.length; i++) {
-        if (stateArray[i][0] != "Null")
-            result += "<div id='" + stateArray[i][0] + "'>";
+        if (stateArray[i][0] != 'Null')
+            result += '<div id=\'' + stateArray[i][0] + '\'>';
     }
     return result;
 }
@@ -74,43 +74,64 @@ function openDivs(stateArray) {
 var statechange;
 var output = '';
 
+function toCamelCase(str) {
+    var arr = str.split(' ');
+    arr[0] = arr[0].toLowerCase();
+
+    return arr.join('');
+}
+
+function element(el_name, el_dependencies, el_type, el_values, el_mandatory) {
+    var result ='';
+    var el_id = toCamelCase(el_name);
+    if (el_type==='string')
+        result = '<p>'+el_name+':<input type=\'text\' name=\''+el_id+'\'></p>';
+    else
+        result = '<element id=\'' + el_id + '\'' + 'dependencies=\'' + el_dependencies + '\' type=\'' + el_type + '\' values = \'' + el_values + '\' mandatory = \''+el_mandatory +' \' ></element>';
+    return result;
+}
+
 while (line = liner.next()) {
     line = line.toString('ascii');
-    if (!first) {
+    //  if (!first) {
 
-        arr_line = line.split(delimiter);
+    arr_line = line.split(delimiter);
 
-        stateA = arr_line[0];
-        stateB = arr_line[1];
-        stateC = arr_line[2];
-        el_name = arr_line[3];
+    stateA = arr_line[0];
+    stateB = arr_line[1];
+    stateC = arr_line[2];
+    el_name = arr_line[3];
+    el_dependencies = arr_line[4];
+    el_type = arr_line[5];
+    el_values = arr_line[6];
+    el_mandatory = arr_line[7];
 
-        if (arr_line[0].length == 0) stateA = "Null";
-        if (arr_line[1].length == 0) stateB = "Null";
-        if (arr_line[2].length == 0) stateC = "Null";
+    if (arr_line[0].length === 0) stateA = 'Null';
+    if (arr_line[1].length === 0) stateB = 'Null';
+    if (arr_line[2].length === 0) stateC = 'Null';
 
 
-        statechange = stateChange(stateA, stateB, stateC, stateA_prev, stateB_prev, stateC_prev);
-        console.log("-->", stateA, stateB, stateC, statechange);
+    statechange = stateChange(stateA, stateB, stateC, stateA_prev, stateB_prev, stateC_prev);
+    console.log('-->', stateA, stateB, stateC, statechange);
 
-        closingDivs = closeDivs(statechange);
-        output += closingDivs;
-        openingDivs = openDivs(statechange);
-        output += openingDivs;
-        output += "<element id='" + el_name + "'></element>";
+    closingDivs = closeDivs(statechange);
+    output += closingDivs;
+    openingDivs = openDivs(statechange);
+    output += openingDivs;
+    output += element(el_name, el_dependencies, el_type, el_values, el_mandatory);
 
-        //update previous states
-        stateA_prev = stateA;
-        stateB_prev = stateB;
-        stateC_prev = stateC;
+    //update previous states
+    stateA_prev = stateA;
+    stateB_prev = stateB;
+    stateC_prev = stateC;
 
-    }
-    first = false;
+    // }
+    //first = false;
     lineNumber++;
 }
 
-statechange = stateChange("Null", "Null", "Null", stateA_prev, stateB_prev, stateC_prev);
-console.log("-->", stateA, stateB, stateC, statechange);
+statechange = stateChange('Null', 'Null', 'Null', stateA_prev, stateB_prev, stateC_prev);
+console.log('-->', stateA, stateB, stateC, statechange);
 
 closingDivs = closeDivs(statechange);
 output += closingDivs;
