c64db12481fbdd0f88f816d278477cb2bc236fa8
dcharlaftis <d.charlaftis@dotbydot.gr>
2016-01-18 15:01:23 +0200

    e

--- csvToHtml.js
+++ csvToHtml.js
@@ -11,6 +11,10 @@ var stateA_prev = "Null",
     stateB_prev = "Null",
     stateC_prev = "Null";
 
+var stateBclosed = true,
+    stateCclosed = true,
+    arr_line;
+
 function writetoHtml(str) {
     fs.appendFile(outFile, str, function(err) {
         if (err) {
@@ -24,58 +28,85 @@ var header = "<html><head><title>Template</title></head><body>"
 writetoHtml(header);
 
 lineReader.eachLine(inFile, function(line, last) {
-    console.log(line);
+    //console.log(line);
     //avoid the first line - title
     if (!first) {
-        var arr_line = line.split(delimiter);
-    }
-    stateA = arr_line[0];
-    stateB = arr_line[1];
-    stateC = arr_line[2];
+        arr_line = line.split(delimiter);
 
-    if (arr_line[0].length == 0) stateA = "Null";
-    if (arr_line[1].length == 0) stateB = "Null";
-    if (arr_line[2].length == 0) stateC = "Null";
+        stateA = arr_line[0];
+        stateB = arr_line[1];
+        stateC = arr_line[2];
+        el_name = arr_line[3];
 
-    var attributes_str= "name='"+ arr_line[3] +"', dependencies = '"+arr_line[4]+ "', type='"+arr_line[5] +"', values='"+arr_line[6]+ "', mandatory='"+arr_line[7]+"'";
-            
-    //stateC
-    if ((stateC != "Null") && (stateC != stateC_prev)) {
-        if (stateC_prev == "Null")
-        {  
-            writetoHtml("<div id='" + stateC + "', " + attributes_str + "></div>");
-        }
-        else
-            writetoHtml("</div><div id='" + stateC + "', " + attributes_str + "></div>");
-    }
+        if (arr_line[0].length == 0) stateA = "Null";
+        if (arr_line[1].length == 0) stateB = "Null";
+        if (arr_line[2].length == 0) stateC = "Null";
 
-    //stateB
-    if ((stateB != "Null") && (stateB != stateB_prev)) {
-        if (stateB_prev == "Null")
-            writetoHtml("<div id='" + stateB + "'>");
-        else
-            writetoHtml("</div><div id='" + stateB + "'>");
-    }
-    else if (stateB == "Null")
+
+
+        var attributes_str = " dependencies = '" + arr_line[4] + "', type='" + arr_line[5] + "', values='" + arr_line[6] + "', mandatory='" + arr_line[7] + "'";
+
+        if (last) {
+            //Close divs for open states A and B . state C closes immediately after it opens
+
+            //close state B
+            if (!stateBclosed)
+                writetoHtml("</div>");
+
+            //close state A
             writetoHtml("</div>");
 
-    //stateA
-    if ((stateA != "Null") && (stateA != stateA_prev)) {
+        } else {
+            //stateA
+            if ((stateA != "Null") && (stateA != stateA_prev)) {
+                if (stateBclosed)
+                    writetoHtml("<div id='" + stateA + "'>");
+                else
+                    writetoHtml("</div><div id='" + stateA + "'>");
+            }
 
+            //stateB
+            if (((stateB != "Null") && (stateB != stateB_prev) && (stateA == stateA_prev))||
+                ((stateB != "Null") && (stateB == stateB_prev) && (stateA != stateA_prev))
+            ) {
+                if (stateB_prev == "Null")
+                    writetoHtml("<div id='" + stateC + "'>");
+                else
+                    writetoHtml("</div><div id='" + stateC + "'>");
 
+                stateCclosed = false;
 
-    } else if (stateA == "Null"){
-      
+            } else if ((stateC == "Null") && (stateC != stateC_prev) && (stateB == stateB_prev)) {
+                writetoHtml("</div>");
+                stateCclosed = true;
+            }
+
+            //stateC
+            if (((stateC != "Null") && (stateC != stateC_prev) && (stateB == stateB_prev))||
+                ((stateC != "Null") && (stateC == stateC_prev) && (stateB != stateB_prev))
+            ) {
+                if (stateC_prev == "Null")
+                    writetoHtml("<div id='" + stateC + "'>");
+                else
+                    writetoHtml("</div><div id='" + stateC + "'>");
+
+                stateCclosed = false;
+
+            } else if ((stateC == "Null") && (stateC != stateC_prev) && (stateB == stateB_prev)) {
+                writetoHtml("</div>");
+                stateCclosed = true;
+            }
+
+            //element name
+            writetoHtml("<div id='" + el_name + "', " + attributes_str + ">");
+            writetoHtml("</div>");
+        }
+
+        //update previous states
+        stateA_prev = stateA;
+        stateB_prev = stateB;
+        stateC_prev = stateC;
     }
-
-    if (last) {
-
-    }
-
-    //update previous states
-    stateA_prev = stateA;
-    stateB_prev = stateB;
-    stateC_prev = stateC;
     first = false;
 });
 
